981108f1cdaa0b89b202efe2580ec79913d375b9
iamrosada <luisrosada2018@gmail.com>
2023-10-04 14:53:24 +0300

    feat: added logger into main function

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
-import "github.com/iamrosada/go-rest-api/router"
+import (
+	"github.com/iamrosada/go-rest-api/config"
+	"github.com/iamrosada/go-rest-api/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
-	//Initialize Route
 
+	logger = config.GetLogger("main")
+	err := config.Init()
+
+	if err != nil {
+		//panic(err)
+		// fmt.Println(err)
+		logger.Errorf("Config Initialization error: %v", err)
+		return
+	}
+	//Initialize Route
 	router.Initialize()
 
 }
