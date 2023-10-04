cafa824ddfa0f4bae98784662c7529720b7ce718
iamrosada <luisrosada2018@gmail.com>
2023-10-04 14:52:37 +0300

    feat: added custom logger

---
+++ config/logger.go
@@ -0,0 +1,63 @@
+package config
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+type Logger struct {
+	debug   *log.Logger
+	info    *log.Logger
+	warning *log.Logger
+	err     *log.Logger
+	write   io.Writer
+}
+
+func NewLogger(prefix string) *Logger {
+	writer := io.Writer(os.Stdout)
+
+	logger := log.New(writer, prefix, log.Ldate|log.Ltime)
+
+	return &Logger{
+		debug:   log.New(writer, "DEBUG: ", logger.Flags()),
+		info:    log.New(writer, "INFO: ", logger.Flags()),
+		warning: log.New(writer, "WARNING: ", logger.Flags()),
+		err:     log.New(writer, "ERROR: ", logger.Flags()),
+		write:   writer,
+	}
+}
+
+// create Non-formatted Logs
+func (l *Logger) Debug(v ...interface{}) {
+	l.debug.Println(v...)
+}
+
+func (l *Logger) Info(v ...interface{}) {
+	l.info.Println(v...)
+}
+
+func (l *Logger) Warning(v ...interface{}) {
+	l.warning.Println(v...)
+}
+
+func (l *Logger) Err(v ...interface{}) {
+	l.err.Println(v...)
+}
+
+// Create Format Enable Logs
+func (l *Logger) Debugf(format string, v ...interface{}) {
+	l.debug.Printf(format, v...)
+}
+
+func (l *Logger) Infof(format string, v ...interface{}) {
+	l.info.Printf(format, v...)
+}
+
+func (l *Logger) Warningf(format string, v ...interface{}) {
+	l.warning.Printf(format, v...)
+}
+
+func (l *Logger) Errorf(format string, v ...interface{}) {
+	l.err.Printf(format, v...)
+}
