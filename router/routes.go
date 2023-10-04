eb3c982bd55a075c93ccbc213b698693bd0efff3
iamrosada <luisrosada2018@gmail.com>
2023-10-04 11:52:50 +0300

    feat: add logic for routes

--- router/routes.go
+++ router/routes.go
@@ -16,5 +16,27 @@ func initializeRoutes(router *gin.Engine) {
 				"message": "GET opening",
 			})
 		})
+
+		v1.PUT("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "PUT opening",
+			})
+		})
+
+		v1.POST("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "POST opening",
+			})
+		})
+		v1.DELETE("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "DELETE opening",
+			})
+		})
+		v1.GET("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "DELETE opening",
+			})
+		})
 	}
 }
