63c1860eef1ec370751bca29a690ca253e81a7eb
iamrosada <luisrosada2018@gmail.com>
2023-10-05 19:58:20 +0300

    feat: add Initialize handler

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	"github.com/iamrosada/go-rest-api/config"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
+func InitializeHendler() {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
