cbbf87a5d21a37f442208fd96c95beabfc854572
iamrosada <luisrosada2018@gmail.com>
2023-10-05 19:59:44 +0300

    feat: add function that call routes on project

--- router/routes.go
+++ router/routes.go
@@ -2,18 +2,29 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	docs "github.com/iamrosada/go-rest-api/docs"
 	"github.com/iamrosada/go-rest-api/handler"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
+	//Initialize Handler
+	handler.InitializeHendler()
+
+	basePath := "/api/v1"
+
+	docs.SwaggerInfo.BasePath = basePath
 
 	//Creating  a groupe of routes
-	v1 := router.Group("/api/v1")
+	v1 := router.Group(basePath)
 	{
-		v1.GET("/opening", handler.ShowOportunitiyHandler)
-		v1.PUT("/opening", handler.UpdateOportunitiyHandler)
-		v1.POST("/opening", handler.CreateOportunitiyHandler)
-		v1.DELETE("/opening", handler.DeleteOportunitiyHandler)
-		v1.GET("/opening/o", handler.ListOportunitiesHandler)
+		v1.GET("/oportunity", handler.ShowOportunityHandler)
+		v1.PUT("/oportunity", handler.UpdateOportunityHandler)
+		v1.POST("/oportunity", handler.CreateOportunityHandler)
+		v1.DELETE("/oportunity", handler.DeleteOportunityHandler)
+		v1.GET("/oportunities", handler.ListOportunitiesHandler)
 	}
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
