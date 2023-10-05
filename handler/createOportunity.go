4614caab92f8fb66530a633bb6e2fae1298e96bb
iamrosada <luisrosada2018@gmail.com>
2023-10-05 19:55:34 +0300

    feat: add create function handler

--- handler/createOportunity.go
+++ handler/createOportunity.go
@@ -4,11 +4,35 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/iamrosada/go-rest-api/schemas"
 )
 
-func CreateOportunitiyHandler(ctx *gin.Context) {
+func CreateOportunityHandler(ctx *gin.Context) {
+	request := CreateOportunityRequest{}
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "POST opening",
-	})
+	ctx.BindJSON(&request)
+
+	//logger.Infof("request received: %+v", request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("Validatione error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	oportunity := schemas.Oportunity{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&oportunity).Error; err != nil {
+		logger.Errorf("error creating oportunity: %+v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error creating oportunity on database")
+		return
+	}
+	sendSucess(ctx, "creating-oportunity", oportunity)
 }
