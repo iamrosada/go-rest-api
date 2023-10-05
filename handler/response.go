67440f6cb434d1ec0ae33a0c8eb000afe00f2b46
iamrosada <luisrosada2018@gmail.com>
2023-10-05 19:59:19 +0300

    feat:  created response for opportunity of type structure

---
+++ handler/response.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func sendError(ctx *gin.Context, code int, msg string) {
+
+	ctx.Header("Content-type", "application")
+	ctx.JSON(code, gin.H{
+		"message":   msg,
+		"errorCode": code,
+	})
+}
+
+func sendSucess(ctx *gin.Context, option string, data interface{}) {
+
+	ctx.Header("Content-type", "application")
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("operation from handler: %s sucessefull", option),
+		"data":    data,
+	})
+}
