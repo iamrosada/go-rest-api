09717170f55b9913ad43ac77907188118b03f5b2
iamrosada <luisrosada2018@gmail.com>
2023-10-05 19:57:43 +0300

    feat: add update opportunity function handler

--- handler/updateOportunity.go
+++ handler/updateOportunity.go
@@ -4,11 +4,69 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/iamrosada/go-rest-api/schemas"
 )
 
-func UpdateOportunitiyHandler(ctx *gin.Context) {
+func UpdateOportunityHandler(ctx *gin.Context) {
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "PUT opening",
-	})
+	request := UpdateOportunityRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("Validation Error %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+
+	}
+
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest,
+			errParamsIsRequired("id", "queryParameters").Error())
+		return
+	}
+
+	oportunity := schemas.Oportunity{}
+	if err := db.First(&oportunity, id).Error; err != nil {
+		sendError(ctx, http.StatusBadRequest,
+			"oportunity not found")
+
+		return
+	}
+
+	if request.Role != "" {
+		oportunity.Role = request.Role
+	}
+
+	if request.Company != "" {
+		oportunity.Company = request.Company
+	}
+
+	if request.Location != "" {
+		oportunity.Location = request.Location
+	}
+
+	if request.Link != "" {
+		oportunity.Link = request.Link
+	}
+
+	if request.Remote != nil {
+		oportunity.Remote = *request.Remote
+	}
+
+	if request.Salary <= 0 {
+		oportunity.Salary = request.Salary
+	}
+
+	//save oportunity
+
+	if err := db.Save(&oportunity).Error; err != nil {
+		logger.Errorf("error updating oportunity: %+v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error updatings oportunity on database")
+		return
+	}
+	sendSucess(ctx, "updating-oportunity", oportunity)
+
 }
