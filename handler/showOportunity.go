0424cbf31f1acc141ccfec452dc941d8c7e1ce90
iamrosada <luisrosada2018@gmail.com>
2023-10-05 19:57:11 +0300

    feat: add show opportunity handler function

--- handler/showOportunity.go
+++ handler/showOportunity.go
@@ -4,11 +4,24 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/iamrosada/go-rest-api/schemas"
 )
 
-func ShowOportunitiyHandler(ctx *gin.Context) {
+func ShowOportunityHandler(ctx *gin.Context) {
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET opening",
-	})
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest,
+			errParamsIsRequired("id", "queryParameters").Error())
+		return
+	}
+
+	oportunity := schemas.Oportunity{}
+	if err := db.First(&oportunity, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "oportunity not found")
+		return
+	}
+
+	sendSucess(ctx, "show-oportunity", oportunity)
 }
